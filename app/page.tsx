902af383e7419da3e6a68b57231c9deca9865824
venkappu777 <venkappu7643@gmail.com>
2024-10-10 21:05:06 +0530

    Add Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import SideBar from "@/components/SideBar";
 import Image from "next/image";
 
 export default function Home() {
@@ -37,7 +38,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy now   rf
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
@@ -48,6 +49,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <SideBar/>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
---
+++ components/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>SideBar component</div>
+  )
+}
+
+export default SideBar
